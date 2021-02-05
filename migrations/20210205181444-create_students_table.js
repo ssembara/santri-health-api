4b078abd0d5e0456d65386d3d7c29cf2f1f276ca
Sebastianus Sembara <sembara9090@gmail.com>
2021-02-06 01:26:29 +0700

    add create_students_table_migration

---
+++ migrations/20210205181444-create_students_table.js
@@ -0,0 +1,50 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Students', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
+      },
+      class_id: {
+        type: Sequelize.DataTypes.INTEGER,
+        allowNull: false,
+        onDelete: "CASCADE",
+        references: {
+          model: "Classes",
+          key: "id",
+          as: "class_id"
+        }
+      },
+      nis: {
+        type: Sequelize.DataTypes.STRING,
+        allowNull: false,
+        unique: true
+      },
+      name: {
+        type: Sequelize.DataTypes.STRING,
+        allowNull: false,
+        unique: true
+      },
+      sex: Sequelize.DataTypes.STRING,
+      birthday: Sequelize.DataTypes.DATE,
+      address: Sequelize.DataTypes.STRING,
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false
+      }
+
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("Students");
+  }
+};
