050cbd0f1698e27cc947648150201eb46d690dfd
Sebastianus Sembara <sembara9090@gmail.com>
2021-02-06 01:14:12 +0700

    create classes_table_migration

---
+++ migrations/20210205180257-create_classes_table.js
@@ -0,0 +1,23 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Classes', {
+      name: Sequelize.DataTypes.STRING,
+      grade: Sequelize.DataTypes.INTEGER,
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false
+      }
+
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("Classes");
+  }
+};
